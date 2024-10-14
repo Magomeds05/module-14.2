44eb0074c7b475f23b7fc4911d7d8774aadd6f0c
Magomeds05 <yusypovm@gmail.com>
2024-10-14 18:06:53 +0300

    Update module 14.2.py

--- module 14.2.py
+++ module 14.2.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-connection = sqlite3.connect("not_telegram2.db")
+connection = sqlite3.connect("not_telegram.db")
 curs = connection.cursor()
 
 curs.execute('''
 CREATE TABLE IF NOT EXISTS Users(
-id INTEGER,
+id INTEGER PRIMACY KEY,
 username TEXT NOT NULL,
 email TEXT NOT NULL,
 age INTEGER,
@@ -14,31 +14,20 @@ balance INTEGER NOT NULL
 ''')
 
 #Заполните её 10 записями:
+
 for i in range(1, 11):
     curs.execute("INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)", (f"User{i}", f"example{i}@gmail.com", f"{i * 10}", "1000"))
 
 #Обновите balance у каждой 2ой записи начиная с 1ой на 500:
-
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User1"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User3"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User5"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User7"))
-curs.execute("UPDATE Users SET balance = ? WHERE username = ? ", (500, "User9"))
+for j in range(1, 11, 2):
+    curs.execute("UPDATE Users SET balance = ? WHERE age = ? ", (500, j*10))
 
 #Удалите каждую 3ую запись в таблице начиная с 1ой:
-
-curs.execute("DELETE FROM Users WHERE username = ?", ("User1",))
-curs.execute("DELETE FROM Users WHERE username = ?", ("User4",))
-curs.execute("DELETE FROM Users WHERE username = ?", ("User7",))
-curs.execute("DELETE FROM Users WHERE username = ?", ("User10",))
+for k in range(1, 11, 3):
+    curs.execute("DELETE FROM Users WHERE username = ?", (k,))
 
 #Сделайте выборку всех записей при помощи fetchall(), где возраст не равен 60 и выведите их в консоль в следующем формате (без id):
-curs.execute("SELECT age FROM Users WHERE age != ?", (60,))
-users = curs.fetchall()
-for j in users:
-    print(j)
-
-curs.execute("DELETE FROM Users WHERE age = ?", (60,))
+curs.execute("SELECT * FROM Users WHERE age != 60")
 
 # Удаление пользователя с id=6
 
